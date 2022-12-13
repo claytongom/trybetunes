f14cee752e03d457aba95c2e9570fc0ee41a91db
claytongom <claytoncnx@gmail.com>
2022-12-13 16:06:49 -0300

    resolução requisito 1

--- src/App.js
+++ src/App.js
@@ -1,8 +1,27 @@
 import React from 'react';
-// init
+import { BrowserRouter, Route } from 'react-router-dom';
+import Login from './pages/Login';
+import Search from './pages/Search';
+import Favorites from './pages/Favorites';
+import NotFound from './pages/NotFound';
+import Album from './pages/Album';
+import Profile from './pages/Profile';
+import ProfileEdit from './pages/ProfileEdit';
+
 class App extends React.Component {
   render() {
-    return (<p>TrybeTunes</p>);
+    return (
+      <BrowserRouter>
+        <p>TrybeTunes</p>
+        <Route exact path="/" component={ Login } />
+        <Route path="/search" component={ Search } />
+        <Route path="/favorites" component={ Favorites } />
+        <Route path="/album/:id" component={ Album } />
+        <Route path="/profile" component={ Profile } />
+        <Route path="/profile/edit" component={ ProfileEdit } />
+        <Route path="*" component={ NotFound } />
+      </BrowserRouter>
+    );
   }
 }
 
