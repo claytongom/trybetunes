bd421dd382fc286446fe58f5e9c36b9c19e02461
claytongom <claytoncnx@gmail.com>
2022-12-14 17:44:32 -0300

    resolução requisito 4

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import { getUser } from '../services/userAPI';
 import Loading from '../pages/Loading';
 
@@ -21,9 +22,12 @@ class Header extends React.Component {
     return (
       <header data-testid="header-component">
         { loading && <Loading /> }
-        <h1 data-testid="header-user-name">
-          { userName }
-        </h1>
+        <h1 data-testid="header-user-name">{ userName }</h1>
+        <nav>
+          <Link data-testid="link-to-search" to="/search">Search</Link>
+          <Link data-testid="link-to-profile" to="/profile">Profile</Link>
+          <Link data-testid="link-to-favorites" to="/favorites">Favorites</Link>
+        </nav>
       </header>
     );
   }
