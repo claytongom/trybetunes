6fb20b4ec2fbb9d67494a084ff16ae6f4a6fdff7
claytongom <claytoncnx@gmail.com>
2022-12-14 18:10:16 -0300

    resolução requisito 5

--- src/pages/Search.jsx
+++ src/pages/Search.jsx
@@ -1,11 +1,39 @@
-import React, { Component } from 'react';
+import React from 'react';
 import Header from '../components/Header';
 
-class Search extends Component {
+class Search extends React.Component {
+  state = {
+    desabledSearch: true,
+  };
+
+  handleSearch = ({ target }) => {
+    const maxLength = 2;
+    const { value } = target;
+    const compareLength = value.length >= maxLength;
+    this.setState({ desabledSearch: !compareLength });
+  };
+
   render() {
+    const { desabledSearch } = this.state;
     return (
       <div data-testid="page-search">
         <Header />
+        <form>
+          <label htmlFor="search-input">
+            <input
+              type="text"
+              data-testid="search-artist-input"
+              onChange={ this.handleSearch }
+            />
+            <button
+              type="button"
+              data-testid="search-artist-button"
+              disabled={ desabledSearch }
+            >
+              Pesquisar
+            </button>
+          </label>
+        </form>
       </div>
     );
   }
