b4310f6c7e4d39584d16dd0f065991d97683101a
claytongom <claytoncnx@gmail.com>
2023-01-11 11:15:23 -0300

    resolução requisito 7

---
+++ src/components/MusicCard.jsx
@@ -0,0 +1,48 @@
+// import React from 'react';
+// import PropTypes from 'prop-types';
+
+// class MusicCard extends React.Component {
+//   render() {
+//     const { trackName, previewUrl } = this.props;
+//     return (
+//       <div>
+//         <h3>{ trackName }</h3>
+//         <audio data-testid="audio-component" src={ previewUrl } controls>
+//           <track kind="captions" />
+//         </audio>
+//       </div>
+//     );
+//   }
+// }
+
+// MusicCard.propTypes = {
+//   trackName: PropTypes.string.isRequired,
+//   previewUrl: PropTypes.string.isRequired,
+// };
+
+// export default MusicCard;
+
+import React from 'react';
+import PropTypes from 'prop-types';
+
+class MusicCard extends React.Component {
+  render() {
+    const { trackName, previewUrl } = this.props;
+
+    return (
+      <div>
+        <p>{ trackName }</p>
+        <audio data-testid="audio-component" src={ previewUrl } controls>
+          <track kind="captions" />
+        </audio>
+      </div>
+    );
+  }
+}
+
+MusicCard.propTypes = {
+  trackName: PropTypes.string.isRequired,
+  previewUrl: PropTypes.string.isRequired,
+};
+
+export default MusicCard;
--- src/pages/Album.jsx
+++ src/pages/Album.jsx
@@ -1,14 +1,58 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import Header from '../components/Header';
+import MusicCard from '../components/MusicCard';
+import getMusics from '../services/musicsAPI';
 
 class Album extends React.Component {
+  state = {
+    trackSongs: [],
+    artistName: '',
+    collectionName: '',
+  };
+
+  async componentDidMount() {
+    const { match: { params: { id } } } = this.props;
+    const musics = await getMusics(id);
+    console.log(musics);
+    this.setState({
+      artistName: musics[0].artistName,
+      collectionName: musics[0].collectionName,
+      trackSongs: musics,
+    });
+  }
+
   render() {
+    const {
+      artistName,
+      collectionName,
+      trackSongs } = this.state;
+
     return (
       <div data-testid="page-album">
         <Header />
+        <div>
+          <h4 data-testid="artist-name">{ artistName }</h4>
+          <h3 data-testid="album-name">{ collectionName }</h3>
+          {trackSongs.slice(1).map((track) => (
+            <MusicCard
+              key={ track.trackId }
+              trackName={ track.trackName }
+              previewUrl={ track.previewUrl }
+            />
+          ))}
+        </div>
       </div>
     );
   }
 }
+
+Album.propTypes = {
+  match: PropTypes.shape({
+    params: PropTypes.shape({
+      id: PropTypes.string.isRequired,
+    }).isRequired,
+  }).isRequired,
+};
 
 export default Album;
